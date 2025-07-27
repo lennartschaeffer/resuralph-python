ee3d6240ce279a3179832cd1f1bca8aa0f2efc1c
lennartschaeffer <ln546006@dal.ca>
2025-07-27 17:21:37 -0300

    Initial commit

---
+++ test/resuralph-python.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ResuralphPython from '../lib/resuralph-python-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/resuralph-python-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ResuralphPython.ResuralphPythonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
